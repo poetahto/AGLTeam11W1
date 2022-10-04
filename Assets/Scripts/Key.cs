9a20377b2acbb82233c7131ae0ab1f0b648d7556
James Maron <jamesmaron87@gmail.com>
2022-10-03 22:09:31 -0700

    Keys and Doors

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -7,7 +7,20 @@ public class Key : MonoBehaviour
     [SerializeField]
     private int id;
 
+    [SerializeField]
+    private Transform keyFollowPoint;
+
+    [SerializeField]
+    private GameObject killEffect;
+
+    [SerializeField]
+    private float killAfter = .6f;
+
+    private float _lerp = 2;
+
     private bool _collected = false;
+
+    float _killTime = -1;
 
     public int Id => id;
 
@@ -21,12 +34,28 @@ public class Key : MonoBehaviour
         }
     }
 
+    public void kill(Transform door)
+    {
+        keyFollowPoint = door;
+        _killTime = Time.time;
+    }
+
     void Update()
     {
         if (_collected)
         {
+            this.transform.position = Vector3.Lerp(this.transform.position,
+                keyFollowPoint.position + ((_killTime != -1) ? new Vector3(0,0,0) : new Vector3(1f*id,.25f*id,0)),
+                _lerp * Time.deltaTime);
 
+            if (_killTime != -1 && Time.time > _killTime + killAfter)
+            {
+                Instantiate(killEffect).transform.position = this.transform.position;
+                Destroy(this.gameObject);
+            }
         }
+
+        
         
     }
 }
